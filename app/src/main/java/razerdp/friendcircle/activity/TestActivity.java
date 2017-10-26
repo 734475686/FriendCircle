1af53811af3eef48c37ae5bab13cc433041a7b82
razerdp <razerdp123@gmail.com>
2017-10-27 00:12:33 +0800

    // test

---
+++ app/src/main/java/razerdp/friendcircle/activity/TestActivity.java
@@ -0,0 +1,100 @@
+package razerdp.friendcircle.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import razerdp.friendcircle.R;
+import razerdp.github.com.baseuilib.baseadapter.BaseRecyclerViewAdapter;
+import razerdp.github.com.baseuilib.baseadapter.BaseRecyclerViewHolder;
+
+public class TestActivity extends AppCompatActivity {
+
+    private RecyclerView rvContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        initView();
+    }
+
+    private void initView() {
+        rvContent = (RecyclerView) findViewById(R.id.rv_content);
+
+        final InnerAdapter adapter=new InnerAdapter(this,getTestDatas());
+
+        rvContent.setAdapter(adapter);
+
+        Button refresh= (Button) findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.updateData(getTestDatas());
+            }
+        });
+    }
+
+    private List<String> getTestDatas(){
+        List<String> testData=new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            testData.add("Almost heaven, West Virginia，\n" +
+                                 "西弗吉尼亚，近乎天堂般美妙\n" +
+                                 "Blue Ridge Mountains, Shenandoah River.\n" +
+                                 "蓝岭山脉，珊娜多荷河流\n" +
+                                 "Life is old there, older than the trees,\n" +
+                                 "那里的生命，比树木更古老\n" +
+                                 "Younger than the mountains, blowing like a breeze.\n" +
+                                 "比青山更有活力，像风一样自由\n" +
+                                 "Country road, take me home\n" +
+                                 "故乡的路，带我回家吧\n" +
+                                 "To the place I belong,\n" +
+                                 "回到我的家乡");
+        }
+        return testData;
+    }
+
+
+    private class InnerAdapter extends BaseRecyclerViewAdapter<String> {
+
+        public InnerAdapter(@NonNull Context context, @NonNull List<String> datas) {
+            super(context, datas);
+        }
+
+        @Override
+        protected int getViewType(int position, @NonNull String data) {
+            return 0;
+        }
+
+        @Override
+        protected int getLayoutResId(int viewType) {
+            return R.layout.item_test;
+        }
+
+        @Override
+        protected BaseRecyclerViewHolder getViewHolder(ViewGroup parent, View rootView, int viewType) {
+            return new InnerViewHolder(rootView, viewType);
+        }
+    }
+
+    private class InnerViewHolder extends BaseRecyclerViewHolder<String> {
+
+
+        public InnerViewHolder(View itemView, int viewType) {
+            super(itemView, viewType);
+        }
+
+        @Override
+        public void onBindData(String data, int position) {
+
+        }
+    }
+}
